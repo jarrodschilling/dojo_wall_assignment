18556525a702ec7cae802ebb651ebf366799fbff
jarrodschilling <jarrodschilling@gmail.com>
2023-11-18 17:26:58 -0700

    ready to submit

--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -8,6 +8,7 @@ def wall():
     if 'user_id' not in session:
         return redirect('/')
     posts = Post.all_posts()
+    print(posts[0])
     return render_template('wall.html', posts=posts)
 
 @app.route('/post', methods=['POST'])
