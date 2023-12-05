8711db2c24164ede378f8a6c1bda40d6f561325d
jarrodschilling <jarrodschilling@gmail.com>
2023-12-05 11:06:27 -0700

    formatting wall

--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import render_template, redirect, session, request, flash
 from flask_app.models.post import Post
+from datetime import datetime
 
 
 @app.route('/wall')
@@ -8,7 +9,7 @@ def wall():
     if 'user_id' not in session:
         return redirect('/')
     posts = Post.all_posts()
-    print(posts[0])
+    print(posts[0].created_at.strftime('%b %d'))
     return render_template('wall.html', posts=posts)
 
 @app.route('/post', methods=['POST'])
